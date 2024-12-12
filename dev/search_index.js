13965cecef2c2de7ccb2c7bc414b655035c74f2a
Documenter.jl <documenter@juliadocs.github.io>
2024-12-12 18:43:58 +0000

    build based on 80f2f32

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ScalarWaveFVM","category":"page"},{"location":"#ScalarWaveFVM","page":"Home","title":"ScalarWaveFVM","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ScalarWaveFVM.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ScalarWaveFVM]","category":"page"}]
+}
